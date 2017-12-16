0f1a1a4bb80b60b70adf3f8e2ecd6816130d3242
LOUNICI ALI <lounici_s@yahoo.fr>
2017-12-16 21:11:28 +0100

    version2

--- app.R/app.R
+++ app.R/app.R
@@ -1,19 +1,40 @@
 ui <- fluidPage(
   
-  title= "Application - Test",
+  titlePanel(h4("Application Shiny",align="center")),
   
   fixedRow(
     #On créer une liste déroulante qui s'appelle "patient"
+    
+    
+    sidebarPanel(
     selectInput("patient", "selectionnez un patient", listeT),
+    br(),
     actionButton("voirPatients", "Voir tous les patients")
+    
   ),
   
-  fixedRow(
-    #On créer un tableau qui contiendra nos résultats
-    DT::dataTableOutput("resultPatient"),
-    DT::dataTableOutput("test")
+ 
+
+# Main panel for displaying outputs ----
+mainPanel(
+  
+  # Output: Tabset w/ plot, summary, and table ----
+  tabsetPanel(type = "tabs",
+              
+              tabPanel("Table", tableOutput("table")),
+              tabPanel("Plot", plotOutput("plot"))
   )
+  
+),
+fixedRow(
+  #On créer un tableau qui contiendra nos résultats
+  DT::dataTableOutput("test")
 )
+
+
+)
+)
+
 
 server <- function(input, output) {
   
@@ -24,13 +45,13 @@ server <- function(input, output) {
   
   # Dans output$resultPaitent on va mettre un tableau.
   # getPatient retournera notre tableau.
-  output$resultPatient <- DT::renderDataTable(
+  output$table <- renderTable({
     getPatient(input$patient)
-  )
+  })
   
   observeEvent(input$voirPatients, {
     hide("resultPatient")
-    output$test <- DT::renderDataTable(Patients %>% select(title, organism))
+    output$test <- DT::renderDataTable(patients %>% select(title, organism))
   })
   
   
